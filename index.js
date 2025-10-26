5198c37efe8e33a1cbab87d9dbf14ac05b6a9a11
AlinaSlp <a.slipenka@gmail.com>
2025-10-26 13:41:46 +0000

    Deploying to gh-pages from @ khlivniukoleksandr/Practice-JS@f96013db3be5b466fd6224d5cd5814a31dba304d 🚀

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-import"./assets/styles-JE8YjOlG.js";import{a as e}from"./assets/vendor-N5iQpiFS.js";function c(){const t=document.querySelector(".categories__btn");t&&t.classList.add("categories__btn--active")}const a="https://dummyjson.com/",n={CATEGORIES:"products/category-list"};e.defaults.baseURL=a;async function r(){const{data:t}=await e(`${n.CATEGORIES}`);return t}const i={categoryList:document.querySelector(".categories")};function u(t){const o=t.map(s=>`<li class="categories__item">
-   <button class="categories__btn" type="button">${s}</button>
- </li>`).join("");i.categoryList.innerHTML=o}async function g(){try{const t=await r();u(["All",...t]),c()}catch(t){console.log(t)}}g();
+import"./assets/styles-JE8YjOlG.js";import{a as o}from"./assets/vendor-N5iQpiFS.js";function n(){const t=document.querySelector(".categories__btn");t&&t.classList.add("categories__btn--active")}const e="https://dummyjson.com/",r={CATEGORIES:"products/category-list",PRODUCTS:"products"};o.defaults.baseURL=e;async function i(){const{data:t}=await o(`${r.CATEGORIES}`);return t}async function u(t){const{data:s}=await o(`${r.PRODUCTS}`,{params:{limit:12,skip:(t-1)*12}});return s}const a={categoryList:document.querySelector(".categories"),productList:document.querySelector(".products")};function p(t){const s=t.map(c=>`<li class="categories__item">
+   <button class="categories__btn" type="button">${c}</button>
+ </li>`).join("");a.categoryList.innerHTML=s}function d(t){const s=t.products.map(c=>`
+    <li class="products__item" data-id="${c.id}">
+    <img class="products__image" src="${c.thumbnail}" alt=""/>
+    <p class="products__title">${c.title}</p>
+    <p class="products__brand"><span class="products__brand--bold">Brand:${c.brand}</span></p>
+    <p class="products__category">Category: ${c.category}</p>
+    <p class="products__price">Price: ${c.price}$</p>
+ </li>`).join("");a.productList.innerHTML=s}let l=1;async function _(){try{const t=await i();p(["All",...t]),n()}catch(t){console.log(t)}}async function g(){try{const t=await u(l);d(t)}catch(t){console.log(t)}}_();g();
 //# sourceMappingURL=index.js.map
